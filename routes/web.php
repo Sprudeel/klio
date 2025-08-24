09ffd01a1bd49bf1d833fec17193810c734c6cbb
lschafer <lschafer@ethz.ch>
2025-08-24 22:07:42 +0200

    add all Routes needed

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AssignmentController;
+use App\Http\Controllers\SubmissionController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +17,41 @@ Route::get('/login', function () {
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware('auth')->group(function () {
+
+    // Dashboard / Profile
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+    Route::get('/profile',   [UserController::class, 'profile'])->name('profile');
+    Route::patch('/profile', [UserController::class, 'updateProfile'])->name('profile.update');
+    Route::patch('/password', [UserController::class, 'changePassword'])->name('password.change');
+
+    // Assignments
+    Route::get('/assignments', [AssignmentController::class, 'index'])->name('assignments.index');
+    Route::get('/assignments/create', [AssignmentController::class, 'create'])->name('assignments.create');
+    Route::post('/assignments', [AssignmentController::class, 'store'])->name('assignments.store');
+    Route::get('/assignments/{assignment}', [AssignmentController::class, 'show'])->name('assignments.show');
+    Route::get('/assignments/{assignment}/edit', [AssignmentController::class, 'edit'])->name('assignments.edit');
+    Route::patch('/assignments/{assignment}', [AssignmentController::class, 'update'])->name('assignments.update');
+    Route::post('/assignments/{assignment}/close', [AssignmentController::class, 'close'])->name('assignments.close');
+    Route::post('/assignments/{assignment}/open',  [AssignmentController::class, 'open'])->name('assignments.open');
+    Route::delete('/assignments/{assignment}', [AssignmentController::class, 'destroy'])->name('assignments.destroy');
+
+    // Submissions
+    Route::get('/assignments/{assignment}/submissions', [SubmissionController::class, 'index'])->name('submissions.index');
+    Route::post('/assignments/{assignment}/submissions', [SubmissionController::class, 'store'])->name('submissions.store');
+
+    Route::get('/submissions/{submission}', [SubmissionController::class, 'show'])->name('submissions.show');
+    Route::get('/submissions/{submission}/download', [SubmissionController::class, 'download'])->name('submissions.download');
+    Route::delete('/submissions/{submission}', [SubmissionController::class, 'destroy'])->name('submissions.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get(
+        '/assignments/{assignment}/submissions/merged.pdf',
+        [SubmissionController::class, 'mergedPdf']
+    )->name('assignments.merged-pdf');
+});
+
+//  public intake page by assignment code:
+Route::get('/a/{assignment:code}', [SubmissionController::class, 'createPublic'])->name('assignments.intake');
+Route::post('/a/{assignment:code}', [SubmissionController::class, 'store'])->name('assignments.submit');
